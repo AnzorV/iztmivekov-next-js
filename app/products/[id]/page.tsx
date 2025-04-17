ea9b5c919ed16c7e536669c90c443bac300d598c
AnzorV <anzorv@gmail.com>
2025-04-17 13:57:51 +0100

    master

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,9 @@
-export default function ProductPage({ params: { id } }: { params: { id: string } } ) {
-    return <p>Product {id}</p>
+import { prisma } from "@/prisma/prisma-client"
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } } ) {
+    const product = await prisma.product.findFirst({ where: {id: Number(id)} });
+
+    if (!product) return <p>Product not found</p>;
+
+    return <p>Product {product?.name}</p>;
 }
