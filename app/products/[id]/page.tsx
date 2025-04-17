2778ceb40ee3506e1a05aff4f671c4cff5aa0fc5
AnzorV <anzorv@gmail.com>
2025-04-17 13:58:30 +0100

    master

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { prisma } from "@/prisma/prisma-client"
+import { notFound } from "next/navigation";
 
 export default async function ProductPage({ params: { id } }: { params: { id: string } } ) {
     const product = await prisma.product.findFirst({ where: {id: Number(id)} });
 
-    if (!product) return <p>Product not found</p>;
+    if (!product) {
+        return notFound();
+    }
 
     return <p>Product {product?.name}</p>;
 }
