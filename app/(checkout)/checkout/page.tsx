96a789a3b1d0a3e225441781bfba411d43f6e14f
AnzorV <anzorv@gmail.com>
2025-06-10 18:31:49 +0100

    aster

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,5 +1,12 @@
-import { CheckoutItemDetails, Container, Title, WhiteBlock } from "@/shared/components/shared";
-import { Input, Textarea } from "@/shared/components/ui";
+import {
+  CheckoutItem,
+  CheckoutItemDetails,
+  Container,
+  Title,
+  WhiteBlock,
+} from "@/shared/components/shared";
+import { Button, Input, Textarea } from "@/shared/components/ui";
+import { Package, Percent, Truck } from "lucide-react";
 
 export default function CheckoutPage() {
   return (
@@ -11,7 +18,13 @@ export default function CheckoutPage() {
 
       <div className="flex gap-10">
         <div className="flex flex-col gap-10 flex-1 mb-20">
-          <WhiteBlock title="1. Корзина">1223344443</WhiteBlock>
+          <WhiteBlock title="1. Корзина">
+         <div className="flex flex-col gap-5">
+             <CheckoutItem id={1} imageUrl={'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp'} details="Большая Конфета Россия Классическая с присыпкой в шоколаде, 200 г" name="Чоризо Фреш" price={216} quantity={3} />
+                    <CheckoutItem id={1} imageUrl={'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp'} details="Большая Конфета Россия Классическая с присыпкой в шоколаде, 200 г" name="Чоризо Фреш" price={216} quantity={3} />
+
+         </div>
+          </WhiteBlock>
 
           <WhiteBlock title="2. Персональные данные">
             <div className="grid grid-cols-2 gap-5">
@@ -26,31 +39,64 @@ export default function CheckoutPage() {
             </div>
           </WhiteBlock>
 
-
-           <WhiteBlock title="3. Адрес Доставки">
+          <WhiteBlock title="3. Адрес Доставки">
             <div className="flex flex-col gap-5">
-                <Input name="firstName" className="text-base" placeholder="Введите адресс..." />
-                <Textarea
+              <Input
+                name="firstName"
+                className="text-base"
+                placeholder="Введите адресс..."
+              />
+              <Textarea
                 className="text-base"
                 placeholder="Комментарий к заказу"
-                    rows={5}
-                    />
+                rows={5}
+              />
             </div>
           </WhiteBlock>
         </div>
 
         <div className="w-[450px]">
-            <WhiteBlock className="p-6 sticky top-4">
-                <div className="flex flex-col gap-1">
-                  <span className="text-xl">Итого</span>
-                  <span className="text-[34px] font-extrabold">3506 ₽</span>
+          <WhiteBlock className="p-6 sticky top-4">
+            <div className="flex flex-col gap-1">
+              <span className="text-xl">Итого</span>
+              <span className="text-[34px] font-extrabold">3506 ₽</span>
+            </div>
+
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Package size={18} className="mr-2 text-gray-400" />
+                  Стоимость товаров:
                 </div>
+              }
+              value="3000 ₽"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Percent size={18} className="mr-2 text-gray-400" />
+                  Налоги:
+                </div>
+              }
+              value="3000 ₽"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Truck size={18} className="mr-2 text-gray-400" />
+                  Доставка:
+                </div>
+              }
+              value="3000 ₽"
+            />
 
-                <CheckoutItemDetails title="Стоимость товаров:" value="3000 ₽" />
-                                <CheckoutItemDetails title="Налоги:" value="3000 ₽" />
-                <CheckoutItemDetails title="Доставка:" value="3000 ₽" />
-
-            </WhiteBlock>
+            <Button
+              type="submit"
+              className="w-full h-14 rounded-2xl mt-6 text-base font-bold"
+            >
+              Перейти к оплате
+            </Button>
+          </WhiteBlock>
         </div>
       </div>
     </Container>
--- shared/components/shared/index.ts
+++ shared/components/shared/index.ts
@@ -17,4 +17,6 @@ export { CountIconButton } from './count-icon-button'
 export { ProductForm } from './product-form';
 export { WhiteBlock } from './white-block';
 export { CheckoutItemDetails } from './checkout-item-details';
+export { CheckoutItem } from './checkout-item';
+export { CheckoutItemSkeleton } from './checkbox-item-skeleton'
 export * from './modals'
