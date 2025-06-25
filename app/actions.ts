5247999219775673f3e8f66b0033a6f74957b5c9
AnzorV <anzorv@gmail.com>
2025-06-25 17:08:31 +0100

    master

--- app/actions.ts
+++ app/actions.ts
@@ -1,26 +1,83 @@
-'use server';
+"use server";
 
 import { prisma } from "@/prisma/prisma-client";
+import { PayOrderTemplate } from "@/shared/components";
 import { CheckoutFormValues } from "@/shared/constants";
+import { sendEmail } from "@/shared/lib";
 import { OrderStatus } from "@prisma/client";
+import { cookies } from "next/headers";
 
 export async function createOrder(data: CheckoutFormValues) {
-    console.log(data);
+  try {
+    const cookieStore = await cookies();
+    const cartToken = cookieStore.get("cartToken")?.value;
 
-    const token = '123';
+    if (!cartToken) {
+      throw new Error("Cart token not found");
+    }
 
-    await prisma.order.create({
-        data: {
-            token,
-            totalAmount: 1500,
-            status: OrderStatus.PENDING,
-            items: [],
-            fullName: data.firstName + ' ' + data.lastName,
-            email: data.email,
-            phone: data.phone,
-            address: data.address,
-            comment: data.comment,
-        }
-    })
-    return 'https://www.youtube.com/watch?v=GUwizGbY4cc&t=64947s';
+    const userCart = await prisma.cart.findFirst({
+      include: {
+        user: true,
+        items: {
+          include: {
+            ingredients: true,
+            productItem: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!userCart) {
+      throw new Error("Cart not found");
+    }
+    if (userCart?.totalAmount == 0) {
+      throw new Error("Cart is empty");
+    }
+    /* Создаем заказ */
+    const order = await prisma.order.create({
+      data: {
+        token: cartToken,
+        fullName: data.firstName + " " + data.lastName,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        comment: data.comment,
+        totalAmount: userCart.totalAmount,
+        status: OrderStatus.PENDING,
+        items: JSON.stringify(userCart.items),
+      },
+    });
+
+    await prisma.cart.update({
+      where: {
+        id: userCart.id,
+      },
+      data: {
+        totalAmount: 0,
+      },
+    });
+
+    await prisma.cartItem.deleteMany({
+      where: {
+        cartId: userCart.id,
+      },
+    });
+
+    await sendEmail(
+      data.email,
+      "Iztmivekov / Оплатите Заказ #" + order.id,
+      PayOrderTemplate({
+        orderId: order.id,
+        totalAmount: order.totalAmount,
+        paymentUrl: "https://www.youtube.com/watch?v=GUwizGbY4cc&t=66507s",
+      })
+    );
+  } catch (err) {
+    console.log('[CreateOrder] Server error', err);
+  }
 }
