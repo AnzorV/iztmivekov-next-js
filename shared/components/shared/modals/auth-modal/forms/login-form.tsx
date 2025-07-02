5b99132f0fcf9a87e9a65e4a91157acdf3a25a4e
AnzorV <anzorv@gmail.com>
2025-07-02 17:35:56 +0100

    master

---
+++ shared/components/shared/modals/auth-modal/forms/login-form.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { formLoginSchema, TFormLoginValues } from "./schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Title } from "../../../title";
+
+interface Props {
+  onClose?: VoidFunction;
+}
+
+export const LoginForm: React.FC = () => {
+  const form = useForm<TFormLoginValues>({
+    resolver: zodResolver(formLoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: TFormLoginValues) => {
+    console.log(data);
+  };
+
+  return (
+    <FormProvider {...form}>
+      <form
+        className="flex flex-col gap-5"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="flex justify-between items-center">
+          <div className="mr-2">
+            <Title text="Вход в аккаунт" size="md" className="font-bold" />
+            <p className="text-gray-400">
+              Введите свою почту, чтобы войти в свой аккаунт
+            </p>
+          </div>
+          <img
+            src="/assets/images/phone-icon.png"
+            alt="phone-icon"
+            width={60}
+            height={60}
+          />
+        </div>
+      </form>
+    </FormProvider>
+  );
+};
